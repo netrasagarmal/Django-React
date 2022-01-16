4023f62b70293eb88f257410fa0efaa07158cded
Sagar Patil <55647831+netrasagarmal@users.noreply.github.com>
2022-01-16 21:52:24 +0530

    sample view

--- quotes/subapp/views.py
+++ quotes/subapp/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from . models import *
 from rest_framework.response import Response
 from . serializer import *
+import json
 # Create your views here.
 
 class ReactView(APIView):
@@ -20,3 +21,40 @@ class ReactView(APIView):
 		if serializer.is_valid(raise_exception=True):
 			serializer.save()
 			return Response(serializer.data)
+
+class Sample(APIView):
+    serializer_class = sampleSerializer
+  
+    def get(self, request):
+        data1 = {
+            "user": {
+                "name": "Sagar Patil",
+                "age": 21,
+                "Place": "Baramati",
+                "Blood group": "O-ve"
+            }
+        }
+        
+        # Serializing json
+        res1 = json.dumps( data1 )
+        return Response(res1)
+  
+    def post(self, request):
+  
+        serializer = sampleSerializer(data=request.data)
+        # Data to be written 
+        data1 = {
+            "user": {
+                "name": "satyam kumar",
+                "age": 21,
+                "Place": "Patna",
+                "Blood group": "O+"
+            }
+        }
+        
+        # Serializing json
+        res1 = json.dumps( data1 )
+        if serializer.is_valid(raise_exception=True):
+            print(serializer.data['userName']+" age is : "+serializer.data['age'])
+            print(res1)
+            return  Response(res1)
